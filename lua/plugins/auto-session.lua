c6cca44cf08e70cbf241a71bb282440a6b53af24
Alex Veden <i@alexveden.com>
2024-05-04 20:00:15 +0400

    WIP auto-session / restore + neo tree fix

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,15 @@
+return {
+    'rmagatti/auto-session',
+    dependencies = { 'nvim-neo-tree/neo-tree.nvim' },
+    config = function()
+        -- auto-session specifics
+        vim.opt.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+
+        require('auto-session').setup {
+            log_level = 'error',
+            auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+            pre_save_cmds = { 'Neotree close' },
+            post_restore_cmds = { 'Neotree filesystem show' },
+        }
+    end,
+}
