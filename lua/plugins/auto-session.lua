dc2677b9d8dc635e2b9700b1f6437cab317eb1a9
Alex Veden <i@alexveden.com>
2024-05-05 12:35:19 +0400

    WIP auto-session enabled only if .git exists

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -2,14 +2,24 @@ return {
     'rmagatti/auto-session',
     dependencies = { 'nvim-neo-tree/neo-tree.nvim' },
     config = function()
-        -- auto-session specifics
-        vim.opt.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+        local git_dir = vim.fn.getcwd() .. "/.git"
 
-        require('auto-session').setup {
-            log_level = 'error',
-            auto_session_suppress_dirs = { '~/', '~/code', '~/Downloads', '/', "~/cloud/"},
-            pre_save_cmds = { 'Neotree close' },
-            -- post_restore_cmds = { 'Neotree filesystem show' },
-        }
+        -- Only allow session saving in working dir with .git (projects)
+        if vim.fn.isdirectory(git_dir) ~= 1 then
+            require('auto-session').setup {
+                auto_session_enabled = false,
+            }
+        else
+            -- auto-session specifics
+            vim.opt.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+
+            require('auto-session').setup {
+                auto_session_enabled = true,
+                log_level = 'error',
+                auto_session_suppress_dirs = { '~/', '~/code', '~/Downloads', '/', "~/cloud/" },
+                pre_save_cmds = { 'Neotree close' },
+                -- post_restore_cmds = { 'Neotree filesystem show' },
+            }
+        end
     end,
 }
