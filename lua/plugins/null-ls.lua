b59aeaa2a22f62506af8f2251d46e92613e0b6fe
Alex Veden <i@alexveden.com>
2024-05-07 12:38:18 +0400

    formatting

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,24 +1,25 @@
 return {
-    --"nvimtools/none-ls.nvim",  -- WTF! no flake8 tool!!!!
-    "jose-elias-alvarez/null-ls.nvim",
-    lazy=false,
-    opts = function(_, config)
-        -- config variable is the default configuration table for the setup function call
-        local null_ls = require "null-ls"
+  --"nvimtools/none-ls.nvim",  -- WTF! no flake8 tool!!!!
+  'jose-elias-alvarez/null-ls.nvim',
+  lazy = false,
+  opts = function(_, config)
+    -- config variable is the default configuration table for the setup function call
+    local null_ls = require 'null-ls'
 
-        -- Check supported formatters and linters
-        -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-        -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-        config.sources = {
-            -- Set a formatter
-            null_ls.builtins.formatting.stylua,
-            null_ls.builtins.diagnostics.flake8,
-            null_ls.builtins.code_actions.gitsigns,
-            null_ls.builtins.diagnostics.checkmake,
-            null_ls.builtins.formatting.black.with({
-                extra_args = { "--preview" },
-            }),
-        }
-        return config -- return final config table
-    end
+    -- Check supported formatters and linters
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    config.sources = {
+      -- Set a formatter
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.diagnostics.flake8,
+      null_ls.builtins.code_actions.gitsigns,
+      null_ls.builtins.diagnostics.checkmake,
+      null_ls.builtins.formatting.black.with {
+
+        extra_args = { '--preview' },
+      },
+    }
+    return config -- return final config table
+  end,
 }
