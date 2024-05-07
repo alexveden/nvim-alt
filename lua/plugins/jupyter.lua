4cf568ea703276aa2567fa38b8f603fbe2150994
Alex Veden <i@alexveden.com>
2024-05-07 12:39:03 +0400

    Jupyter switched to Jupynium

--- lua/plugins/jupyter.lua
+++ lua/plugins/jupyter.lua
@@ -1,75 +1,4 @@
 return {
-  {
-    'vhyrro/luarocks.nvim',
-    priority = 1001, -- this plugin needs to run before anything else
-    opts = {
-      rocks = { 'magick' },
-    },
-  },
-  {
-    '3rd/image.nvim',
-    dependencies = { 'luarocks.nvim' },
-    config = function()
-      require('image').setup {
-        backend = 'ueberzug',
-        integrations = {
-          markdown = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
-          },
-          neorg = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            filetypes = { 'norg' },
-          },
-          html = {
-            enabled = false,
-          },
-          css = {
-            enabled = false,
-          },
-        },
-        max_width = nil,
-        max_height = nil,
-        max_width_window_percentage = nil,
-        max_height_window_percentage = 50,
-        window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-        window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', '' },
-        editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-        tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-        hijack_file_patterns = { '*.png', '*.jpg', '*.jpeg', '*.gif', '*.webp' }, -- render image files as images when opened
-      }
-    end,
-  },
-  {
-    'benlubas/molten-nvim',
-    enabled = true,
-    build = ':UpdateRemotePlugins',
-    init = function()
-      vim.g.molten_image_provider = 'image.nvim'
-      -- vim.g.molten_image_provider = 'none'
-      vim.g.molten_output_win_max_height = 20
-      vim.g.molten_virt_text_output = true
-      vim.g.molten_auto_open_output = true
-      vim.g.molten_output_win_hide_on_leave = false
-    end,
-    keys = {
-      { '<leader>mi', ':MoltenInit<cr>', desc = '[m]olten [i]nit' },
-      {
-        '<leader>mv',
-        ':<C-u>MoltenEvaluateVisual<cr>',
-        mode = 'v',
-        desc = 'molten eval visual',
-      },
-      { '<leader>mr', ':MoltenReevaluateCell<cr>', desc = 'molten re-eval cell' },
-      { '<leader>ml', ':MoltenEvaluateLine<cr>', desc = 'molten re-eval line' },
-    },
-  },
   { -- directly open ipynb files as quarto docuements
     -- and convert back behind the scenes
     -- needs:
@@ -104,6 +33,119 @@ return {
       local nn = require 'notebook-navigator'
       nn.setup {
         repl_provider = 'molten',
+      }
+    end,
+  },
+  {
+    'kiyoon/jupynium.nvim',
+    -- build = 'pip3 install --user .',
+    -- build = "conda run --no-capture-output -n jupynium pip install .",
+    -- enabled = vim.fn.isdirectory(vim.fn.expand "~/miniconda3/envs/jupynium"),
+    config = function()
+      require('jupynium').setup {
+        --- For Conda environment named "jupynium",
+        -- python_host = { "conda", "run", "--no-capture-output", "-n", "jupynium", "python" },
+        python_host = vim.g.python3_host_prog or 'python3',
+
+        default_notebook_URL = 'localhost:8889/nbclassic',
+
+        -- Write jupyter command but without "notebook"
+        -- When you call :JupyniumStartAndAttachToServer and no notebook is open,
+        -- then Jupynium will open the server for you using this command. (only when notebook_URL is localhost)
+        jupyter_command = 'jupyter',
+        --- For Conda, maybe use base environment
+        --- then you can `conda install -n base nb_conda_kernels` to switch environment in Jupyter Notebook
+        -- jupyter_command = { "conda", "run", "--no-capture-output", "-n", "base", "jupyter" },
+
+        -- Used when notebook is launched by using jupyter_command.
+        -- If nil or "", it will open at the git directory of the current buffer,
+        -- but still navigate to the directory of the current buffer. (e.g. localhost:8888/nbclassic/tree/path/to/buffer)
+        notebook_dir = nil,
+
+        -- Used to remember the last session (password etc.).
+        -- e.g. '~/.mozilla/firefox/profiles.ini'
+        -- or '~/snap/firefox/common/.mozilla/firefox/profiles.ini'
+        firefox_profiles_ini_path = nil,
+        -- nil means the profile with Default=1
+        -- or set to something like 'default-release'
+        firefox_profile_name = nil,
+
+        -- Open the Jupynium server if it is not already running
+        -- which means that it will open the Selenium browser when you open this file.
+        -- Related command :JupyniumStartAndAttachToServer
+        auto_start_server = {
+          enable = false,
+          file_pattern = { '*.ju.*' },
+        },
+
+        -- Attach current nvim to the Jupynium server
+        -- Without this step, you can't use :JupyniumStartSync
+        -- Related command :JupyniumAttachToServer
+        auto_attach_to_server = {
+          enable = true,
+          file_pattern = { '*.ju.*', '*.md' },
+        },
+
+        -- Automatically open an Untitled.ipynb file on Notebook
+        -- when you open a .ju.py file on nvim.
+        -- Related command :JupyniumStartSync
+        auto_start_sync = {
+          enable = false,
+          file_pattern = { '*.ju.*', '*.md' },
+        },
+
+        -- Automatically keep filename.ipynb copy of filename.ju.py
+        -- by downloading from the Jupyter Notebook server.
+        -- WARNING: this will overwrite the file without asking
+        -- Related command :JupyniumDownloadIpynb
+        auto_download_ipynb = true,
+
+        -- Automatically close tab that is in sync when you close buffer in vim.
+        auto_close_tab = true,
+
+        -- Always scroll to the current cell.
+        -- Related command :JupyniumScrollToCell
+        autoscroll = {
+          enable = true,
+          mode = 'always', -- "always" or "invisible"
+          cell = {
+            top_margin_percent = 20,
+          },
+        },
+
+        scroll = {
+          page = { step = 0.5 },
+          cell = {
+            top_margin_percent = 20,
+          },
+        },
+
+        -- Files to be detected as a jupynium file.
+        -- Add highlighting, keybindings, commands (e.g. :JupyniumStartAndAttachToServer)
+        -- Modify this if you already have lots of files in Jupytext format, for example.
+        jupynium_file_pattern = { '*.ju.*' },
+
+        use_default_keybindings = true,
+        textobjects = {
+          use_default_keybindings = true,
+        },
+
+        syntax_highlight = {
+          enable = true,
+        },
+
+        -- Dim all cells except the current one
+        -- Related command :JupyniumShortsightedToggle
+        shortsighted = false,
+
+        -- Configure floating window options
+        -- Related command :JupyniumKernelHover
+        kernel_hover = {
+          floating_win_opts = {
+            max_width = 84,
+            border = 'none',
+          },
+        },
       }
     end,
   },
