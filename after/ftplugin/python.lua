a39a490a1fd7d1fdb14a4e65b32276147eef833e
Alex Veden <i@alexveden.com>
2024-05-07 09:26:03 +0400

    jupyter/python individual key bindings

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -2,13 +2,30 @@
 -- use vim.bo - for local options
 -- use vim.api.nvim_buf_set_keymap()
 
--- Include common settings for code 
-vim.cmd('runtime! ftplugin/common_code.lua')
+-- Include common settings for code
+vim.cmd 'runtime! ftplugin/common_code.lua'
 
-vim.api.nvim_buf_set_keymap(0,
-    "n",
-    "<leader>jf",
-    ':lua require("text_objects").f_string_prepend()<CR>',
-    { desc = "add f- prefix for string" })
+-- set specific flag for python/jupytext notebook
+-- check if any '# %%' in file
+--
+local is_jupyter = false
+local all_lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+for _, line in ipairs(all_lines) do
+  if line:match '^# %%%%' then
+    is_jupyter = true
+    break
+  end
+end
 
+-- This might be used by other plugins for conditional actions
+vim.api.nvim_buf_set_var(0, 'python_is_jupyter', is_jupyter)
 
+if is_jupyter then
+  vim.api.nvim_buf_set_keymap(0, 'n', '<C-CR>', "<cmd>lua require('notebook-navigator').run_and_move()<cr>", { desc = '[J]upyter run to next' })
+  vim.api.nvim_buf_set_keymap(0, 'n', '<S-CR>', "<cmd>lua require('notebook-navigator').run_cell()<cr>", { desc = '[J]upyter run in-place' })
+  vim.api.nvim_buf_set_keymap(0, 'n', '<leader>ja', "<cmd>lua require('notebook-navigator').run_all_cells()<cr>", { desc = '[J]upyter [a]ll below' })
+  vim.api.nvim_buf_set_keymap(0, 'n', '<leader>jo', ':noautocmd MoltenEnterOutput<CR>', { desc = '[J]upyter goto [o]utput', silent = true })
+  vim.api.nvim_buf_set_keymap(0, 'v', '<leader>jv', ':<C-u>MoltenEvaluateVisual<CR>gv', { desc = '[J]upyter exec [v]isual', silent = true })
+else
+  vim.api.nvim_buf_set_keymap(0, 'n', '<leader>jf', ':lua require("text_objects").f_string_prepend()<CR>', { desc = 'add f- prefix for string' })
+end
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,6 +3,17 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local ftype_base = require("lualine.components.filetype")
+
+        local ftype = function()
+            local status, is_jupyter = pcall(vim.api.nvim_buf_get_var, 0, "python_is_jupyter")
+            if status and is_jupyter then
+                return vim.bo.filetype .. " (jupyter)"
+            else
+                return ftype_base.update_status()
+            end
+            
+        end
         require('lualine').setup {
             options = {
                 icons_enabled = true,
@@ -26,7 +37,7 @@ return {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
                 lualine_c = { 'filename' },
-                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_x = { 'encoding', 'fileformat', ftype },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
             },
