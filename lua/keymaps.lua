e2264c354a252c3f2fae95a67ddc3d9d8bd9e241
Alex Veden <i@alexveden.com>
2024-05-09 14:15:47 +0400

    refactored autocommands folder

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,20 +5,6 @@
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
-
 -------------------------------------------------------------------------------
 --
 -- Navigation keys
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -22,7 +22,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  import = 'plugins',
-  change_detection = { enabled = false }
+  {import = 'plugins'},
+  {import = 'autocommands'},
 })
 -- vim: ts=2 sts=2 sw=2 et
