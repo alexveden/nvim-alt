bdfff4902a38bf3c98a2fe743e37b615bbc8096d
Alex Veden <i@alexveden.com>
2024-05-10 13:45:55 +0400

    git log/lazy git window implemented

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,60 +1,85 @@
 return {
-    'akinsho/toggleterm.nvim',
-    version = '*',
-    config = function()
-        require('toggleterm').setup {
-            -- size can be a number or function which is passed the current terminal
-            open_mapping = [[<c-\>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
-        }
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  config = function()
+    require('toggleterm').setup {
+      -- size can be a number or function which is passed the current terminal
+      open_mapping = [[<c-\>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
+    }
 
-        -- A termical for lazy git
-        local Terminal  = require('toggleterm.terminal').Terminal
-        local lazygit = Terminal:new {
-            cmd = 'lazygit',
-            dir = 'git_dir',
-            direction = 'float',
-            float_opts = {
-                border = 'double',
-            },
-            -- function to run on opening the terminal
-            on_open = function(term)
-                vim.cmd 'startinsert!'
-                vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
-                vim.api.nvim_buf_set_keymap(term.bufnr, 't', '`', '<cmd>close<CR>', { noremap = true, silent = true })
-                vim.api.nvim_buf_set_keymap(term.bufnr, 'n', '`', '<cmd>close<CR>', { noremap = true, silent = true })
-            end,
-            -- function to run on closing the terminal
-            on_close = function(term)
-                vim.cmd 'startinsert!'
-            end,
-        }
-        function _G._lazygit_toggle()
-            lazygit:toggle()
-        end
-        vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true, desc="Lazygit" })
+    -- A termical for lazy git
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new {
+      cmd = 'lazygit',
+      dir = 'git_dir',
+      direction = 'float',
+      float_opts = {
+        border = 'double',
+      },
+      -- function to run on opening the terminal
+      on_open = function(term)
+        vim.cmd 'startinsert!'
+        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(term.bufnr, 't', '`', '<cmd>close<CR>', { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', '`', '<cmd>close<CR>', { noremap = true, silent = true })
+      end,
+      -- function to run on closing the terminal
+      on_close = function(term)
+        vim.cmd 'startinsert!'
+      end,
+    }
+    function _G._lazygit_toggle()
+      lazygit:toggle()
+    end
+    vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true, desc = 'Lazygit' })
 
-        local console = Terminal:new {
-            cmd = 'zsh',
-            dir = 'git_dir',
-            direction = 'float',
-            float_opts = {
-                border = 'single',
-            },
-            -- function to run on opening the terminal
-            on_open = function(term)
-                vim.cmd 'startinsert!'
-                vim.api.nvim_buf_set_keymap(term.bufnr, 't', '`', '<cmd>close<CR>', { noremap = true, silent = true })
-                vim.api.nvim_buf_set_keymap(term.bufnr, 'n', '`', '<cmd>close<CR>', { noremap = true, silent = true })
-            end,
-            -- function to run on closing the terminal
-            on_close = function(term)
-                vim.cmd 'startinsert!'
-            end,
-        }
-        function _G._console_toggle()
-            console:toggle()
-        end
+    local console = Terminal:new {
+      cmd = 'zsh',
+      dir = 'git_dir',
+      direction = 'float',
+      float_opts = {
+        border = 'single',
+      },
+      -- function to run on opening the terminal
+      on_open = function(term)
+        vim.cmd 'startinsert!'
+        vim.api.nvim_buf_set_keymap(term.bufnr, 't', '`', '<cmd>close<CR>', { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', '`', '<cmd>close<CR>', { noremap = true, silent = true })
+      end,
+      -- function to run on closing the terminal
+      on_close = function(term)
+        vim.cmd 'startinsert!'
+      end,
+    }
+    function _G._console_toggle()
+      console:toggle()
+    end
 
-        vim.api.nvim_set_keymap('n', '`', '<cmd>lua _console_toggle()<CR>', { noremap = true, silent = true, desc="Vimuake console"})
-    end,
+    vim.api.nvim_set_keymap('n', '`', '<cmd>lua _console_toggle()<CR>', { noremap = true, silent = true, desc = 'Vimuake console' })
+
+    function _G._lazygit_log()
+      local fn = vim.api.nvim_buf_get_name(0)
+      local term = Terminal:new {
+        cmd = 'lazygit log -f "' .. fn .. '"',
+        dir = 'git_dir',
+        direction = 'float',
+        float_opts = {
+          border = 'double',
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+          vim.cmd 'startinsert!'
+          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+          vim.api.nvim_buf_set_keymap(term.bufnr, 't', '`', '<cmd>close<CR>', { noremap = true, silent = true })
+          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', '`', '<cmd>close<CR>', { noremap = true, silent = true })
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+          vim.cmd 'startinsert!'
+        end,
+      }
+      term:toggle()
+    end
+    vim.api.nvim_set_keymap('n', '<leader>gl', '<cmd>lua _lazygit_log()<CR>', { noremap = true, silent = true, desc = 'Lazygit' })
+  end,
 }
