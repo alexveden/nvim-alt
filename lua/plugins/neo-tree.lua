3636dab3627df607139be95ef6d7d27370b22517
Alex Veden <i@alexveden.com>
2024-05-14 14:07:57 +0400

    neo-tree modify time col

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -15,25 +15,34 @@ return {
     default_component_configs = {
       indent = { padding = 0 },
       icon = {
-        folder_closed = "",
-        folder_open = "",
-        folder_empty = "",
-        folder_empty_open = "",
-        default = "󰈙",
+        folder_closed = '',
+        folder_open = '',
+        folder_empty = '',
+        folder_empty_open = '',
+        default = '󰈙',
       },
-      modified = { symbol = ""},
+      modified = { symbol = '' },
       git_status = {
         symbols = {
-          added = "",
-          deleted ="",
-          modified = "",
-          renamed = "➜",
-          untracked = "★",
-          ignored = "◌",
-          unstaged = "✗",
-          staged = "✓",
-          conflict = "",
+          added = '',
+          deleted = '',
+          modified = '',
+          renamed = '➜',
+          untracked = '★',
+          ignored = '◌',
+          unstaged = '✗',
+          staged = '✓',
+          conflict = '',
         },
+      },
+      -- If you don't want to use these columns, you can set `enabled = false` for each of them individually
+      file_size = {
+        enabled = true,
+        required_width = 64, -- min width of window required to show this column
+      },
+      last_modified = {
+        enabled = true,
+        required_width = 64, -- min width of window required to show this column
       },
     },
 
@@ -41,10 +50,10 @@ return {
       window = {
         mappings = {
           -- ['\\'] = 'close_window',
-          ["z"] = "expand_all_nodes",
-          ["c"] = "close_node",
-          ["C"] = "copy",
-          ["Z"] = "close_all_nodes",
+          ['z'] = 'expand_all_nodes',
+          ['c'] = 'close_node',
+          ['C'] = 'copy',
+          ['Z'] = 'close_all_nodes',
         },
       },
     },
