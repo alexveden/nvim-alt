3795b1e728c5cbaa49cf2c1cadcbd32fe3728187
Alex Veden <i@alexveden.com>
2024-05-25 07:46:13 +0400

    plugins disabled

--- lua/plugins/quickfix.lua
+++ lua/plugins/quickfix.lua
@@ -1,25 +1,23 @@
-
 local toggle_qf = function()
   local qf_exists = false
   for _, win in pairs(vim.fn.getwininfo()) do
-    if win["quickfix"] == 1 then
+    if win['quickfix'] == 1 then
       qf_exists = true
       break
     end
   end
 
   if qf_exists == true then
-    vim.cmd "cclose"
+    vim.cmd 'cclose'
   else
-    vim.cmd "copen"
+    vim.cmd 'copen'
   end
 end
 
 vim.keymap.set({ 'n', 'v' }, '<leader>q', toggle_qf, { desc = '[q]ickfix window' })
-vim.keymap.set({ 'n'}, ']q', "<cmd>cnext<cr>", { desc = 'Next [q]ickfix' })
-vim.keymap.set({ 'n'}, '[q', "<cmd>cprev<cr>", { desc = 'Previous [q]ickfix' })
+vim.keymap.set({ 'n' }, ']q', '<cmd>cnext<cr>', { desc = 'Next [q]ickfix' })
+vim.keymap.set({ 'n' }, '[q', '<cmd>cprev<cr>', { desc = 'Previous [q]ickfix' })
 
 return {
-  'kevinhwang91/nvim-bqf',
-  filetype = 'qf',
+  { 'kevinhwang91/nvim-bqf', enabled=false, filetype = 'qf' },
 }
