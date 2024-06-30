7e6814f0a24a3ed3810576e9ede6b3bd0dfe4592
Alex Veden <i@alexveden.com>
2024-06-30 18:04:33 +0400

    neo tree copy file path added

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -100,6 +100,45 @@ return {
       --   print(key)
       -- end
     end
+    local function copy_path(state)
+      -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+      -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+      local node = state.tree:get_node()
+      local filepath = node:get_id()
+      local filename = node.name
+      local modify = vim.fn.fnamemodify
+
+      local results = {
+        filepath,
+        modify(filepath, ':.'),
+        modify(filepath, ':~'),
+        filename,
+        modify(filename, ':r'),
+        modify(filename, ':e'),
+      }
+
+      vim.ui.select({
+        '1. Absolute path: ' .. results[1],
+        '2. Path relative to CWD: ' .. results[2],
+        '3. Path relative to HOME: ' .. results[3],
+        '4. Filename: ' .. results[4],
+        '5. Filename without extension: ' .. results[5],
+        '6. Extension of the filename: ' .. results[6],
+      }, { prompt = 'Choose to copy to clipboard:' }, function(choice)
+        if choice then
+          local i = tonumber(choice:sub(1, 1))
+          if i then
+            local result = results[i]
+            vim.fn.setreg('"', result)
+            vim.notify('Copied: ' .. result)
+          else
+            vim.notify 'Invalid selection'
+          end
+        else
+          vim.notify 'Selection cancelled'
+        end
+      end)
+    end
 
     local opts = {
       default_component_configs = {
@@ -140,13 +179,14 @@ return {
         window = {
           mappings = {
             -- ['\\'] = 'close_window',
-            ['<Right>'] = { command = preview_or_expand, desc = 'expand folder or preview', config= { use_float = true, use_image_nvim = false }},
+            ['<Right>'] = { command = preview_or_expand, desc = 'expand folder or preview', config = { use_float = true, use_image_nvim = false } },
             ['<Left>'] = { command = collapse_close_preview, desc = 'close' },
             -- ['<Right>'] = { 'preview', config = { use_float = true, use_image_nvim = false } },
             ['z'] = 'expand_all_nodes',
             ['c'] = 'close_node',
             ['C'] = 'copy',
             ['Z'] = 'close_all_nodes',
+            ['Y'] = copy_path,
             ['<C-h>'] = { command = harpoon_mapping, desc = '[h]arpoon add' },
             -- ['h'] = { command = harpoon_mapping, desc = '[h]arpoon add' },
             ['<C-d>'] = 'noop',
