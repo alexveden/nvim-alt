ad15e792d0ff1eb7f8e7157e2deef8712d62f91e
Alex Veden <i@alexveden.com>
2024-09-25 15:54:56 +0400

    increased tree sitter max size

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,7 +15,7 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
 
         disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
+          local max_filesize = 300 * 1024 -- 100 KB
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
             return true
