ba9a7309729b3103ddd93674a08ae1232a7d9fcb
Alex Veden <i@alexveden.com>
2024-10-08 23:24:56 +0400

    jump next header .md as ctrl+pgup

--- after/ftplugin/common_prose.lua
+++ after/ftplugin/common_prose.lua
@@ -9,3 +9,8 @@ vim.cmd "SoftPencil"
 -- Disable spell check for prose by default
 vim.wo.spell = false
 
+
+-- Jump between headers
+vim.api.nvim_buf_set_keymap(0, 'n', '<C-PageUp>', '<cmd>norm[[<cr>', { desc = 'Jump next header above' })
+vim.api.nvim_buf_set_keymap(0, 'n', '<C-PageDown>', '<cmd>norm]]<cr>', { desc = 'Jump next header below' })
+
