e0e705ba5a98c0512dbe16f354ba6f5851fd83f9
Alex Veden <i@alexveden.com>
2025-01-31 09:44:04 +0400

    c3 fzf

--- after/ftplugin/c3.lua
+++ after/ftplugin/c3.lua
@@ -7,3 +7,7 @@ vim.opt.colorcolumn = '100'
 --set makeprg=c3c\ test\ --test
 --set efm=%s\|%f\|%l\|%c\|%m
 
+vim.bo.tabstop = 4
+
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>fl', ":lua require('c3fzf').c3fzf()<CR>", { desc = '[L]anguage symbols for C3' })
+
