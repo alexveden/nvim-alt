039bfdd5742034b1c3b32abd031b987cbf494de6
Alex Veden <i@alexveden.com>
2025-07-13 17:57:51 +0400

    fix: tmux-navigator - silently remapping C-hjkl

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -1,13 +1,9 @@
 return {
   {
     'christoomey/vim-tmux-navigator', -- Seamless TMUX splits navigation
-    -- overrides `require("mason-lspconfig").setup(...)`
     lazy = false,
-    -- opts = function(_, opts)
-    --   -- add more things to the ensure_installed table protecting against community packs modifying it
-    --   -- opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-    --   --   -- "lua_ls",
-    --   -- })
-    -- end,
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1 -- Disable C-h/C-j/C-k/C-l default mapping
+    end,
   },
 }
