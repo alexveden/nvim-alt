93ebaec20e39d09618e7e5a9876aa4b5eccf65a4
Alex Veden <i@alexveden.com>
2025-07-13 18:01:24 +0400

    keymaps: yazi toggle switched to local mode at file

--- lua/plugins/yazi-nvim.lua
+++ lua/plugins/yazi-nvim.lua
@@ -1,25 +1,20 @@
 return {
   ---@type LazySpec
   {
-    "mikavilpas/yazi.nvim",
-    event = "VeryLazy",
+    'mikavilpas/yazi.nvim',
+    event = 'VeryLazy',
     dependencies = {
       -- check the installation instructions at
       -- https://github.com/folke/snacks.nvim
-      "folke/snacks.nvim"
+      'folke/snacks.nvim',
     },
     keys = {
       -- 👇 in this section, choose your own keymappings!
-      -- {
-      --   "-",
-      --   mode = { "n", "v" },
-      --   "<cmd>Yazi<cr>",
-      --   desc = "Open yazi at the current file",
-      -- },
       {
-        "-",
-        "<cmd>Yazi toggle<cr>",
-        desc = "Resume the last yazi session",
+        '-',
+        mode = { 'n', 'v' },
+        '<cmd>Yazi<cr>',
+        desc = 'Open yazi at the current file',
       },
     },
     ---@type YaziConfig | {}
@@ -36,17 +31,17 @@ return {
       -- - you can customize only some of the keymaps (not all of them)
       -- - you can opt out of all keymaps by setting `keymaps = false`
       keymaps = {
-        show_help = "<f1>",
-        open_file_in_vertical_split = "<c-s>",
+        show_help = '<f1>',
+        open_file_in_vertical_split = '<c-s>',
         -- open_file_in_horizontal_split = "<c-x>",
         -- open_file_in_tab = "<c-t>",
         grep_in_directory = false,
         -- replace_in_directory = "<c-g>",
-        cycle_open_buffers = "<tab>",
-        copy_relative_path_to_selected_files = "<c-y>",
-        send_to_quickfix_list = "<c-q>",
-        change_working_directory = "<c-\\>",
-        open_and_pick_window = "<c-o>",
+        cycle_open_buffers = '<tab>',
+        copy_relative_path_to_selected_files = '<c-y>',
+        send_to_quickfix_list = '<c-q>',
+        change_working_directory = '<c-\\>',
+        open_and_pick_window = '<c-o>',
       },
     },
     -- 👇 if you use `open_for_directories=true`, this is recommended
@@ -55,5 +50,5 @@ return {
       -- vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
     end,
-  }
+  },
 }
