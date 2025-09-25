51b7d412823e6d8f5995eda2f8a10d6082489a66
Alex Veden <i@alexveden.com>
2025-09-25 15:23:03 +0400

    fix: vim open multiuser

--- lua/autocommands/on_active_server.lua
+++ lua/autocommands/on_active_server.lua
@@ -1,18 +1,18 @@
-
 -- Report active VIM window in order to make Alacritty links works
 vim.api.nvim_create_autocmd('FocusGained', {
-	desc = 'Reports actively focused vim server (for Alacritty links/files open)',
-	callback = function()
-	  local server = vim.fn.serverlist() or {""}
-	  local f = assert(io.open("/tmp/nvim_active_window", "w"))
-	  -- print(server[1])
+  desc = 'Reports actively focused vim server (for Alacritty links/files open)',
+  callback = function()
+    local user = vim.env.USER
+    local server = vim.fn.serverlist() or { '' }
+    local f = assert(io.open('/tmp/nvim_active_window_' .. user, 'w'))
+    -- print(server[1])
     f:write(server[1])
     f:close()
 
-	  f = assert(io.open("/tmp/nvim_active_cwd", "w"))
+    f = assert(io.open('/tmp/nvim_active_cwd_' .. user, 'w'))
     f:write(vim.fn.getcwd())
     f:close()
-	end,
+  end,
 })
 
 -- always return empty
